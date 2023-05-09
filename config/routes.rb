9af4aa53a305f5043c263f5f87434f7d6438b517
Rayman <aff79@mail.ru>
2023-05-09 21:24:02 +0300

    Add default rails blog

---
+++ config/routes.rb
@@ -0,0 +1,15 @@
+Rails.application.routes.draw do
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+
+  root "articles#index"
+
+  resources :articles do
+    resources :comments
+  end
+  # get 'articles/index' #??????? нужна ли эта строка?
+  # get "/articles", to: "articles#index"
+  # get "/articles/:id", to: "articles#show"
+
+end
